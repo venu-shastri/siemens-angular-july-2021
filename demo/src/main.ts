2792a32e7e61ccee7c70fdf798fb4ad77c79f7e7
Venugopal  K (C) <vgk@tritechsamvit.com>
2021-07-26 13:31:52 +0530

    Demo Code

---
+++ demo/src/main.ts
@@ -0,0 +1,19 @@
+
+const todo: Readonly<string> = "hello";
+
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/index.html', (req, res) => {
+    res.send('Hello World! from index.html')
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
